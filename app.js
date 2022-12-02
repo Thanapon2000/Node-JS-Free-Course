838dd1d1cf7d260efd166f99c7593ba5ebe5e1e4
Thanapon2000 <inkfly@hotmail.co.th>
2022-12-02 09:48:52 +0700

    Try to push on server

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ import express from 'express';
 import chalk  from 'chalk';
 import debug from 'debug';
 import morgan from 'morgan';
-
-
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express()
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req,res) =>{
     
     res.send("Hello Thanapon Siriwarin");
 })
-app.listen(port, ()=>{
+
+app.listen(port,()=>{
     debug("Listening on port"+chalk.red(port));
 })
