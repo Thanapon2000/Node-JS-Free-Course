b63c9aa64b51978c402711282bfcb7f888a549a6
Thanapon2000 <inkfly@hotmail.co.th>
2022-12-02 11:55:51 +0700

    changes to index.ejs

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ import morgan from 'morgan';
 import path from 'path';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express()
-const port = 3000;
+const PORT = process.env.PORT;
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
@@ -18,6 +19,6 @@ app.get("/", (req,res) =>{
     res.send("Hello Thanapon Siriwarin");
 })
 
-app.listen(port,()=>{
-    debug("Listening on port"+chalk.red(port));
+app.listen(PORT,()=>{
+    debug("Listening on port" + chalk.red(" : "+PORT));
 })
